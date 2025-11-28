621ac895545d5440dfa771271739a15715ae879c
Maby <maby200@gmail.com>
2025-11-27 19:01:14 -0600

    added picture in main page

--- src/pages/saveTheDate.jsx
+++ src/pages/saveTheDate.jsx
@@ -1,10 +1,22 @@
 import "../styles/saveTheDate.css";
+import M_J_pic from "/src/assets/main_pic.png";
 
 const SaveTheDate = () => {
     return (
         <>
             <div className="save-the-date">
-                <h1 style={{fontFamily:"Great Vibes"}}>Maby & Joel</h1>
+                <div className="couple-names">
+                    <h1 style={{fontFamily:"Great Vibes"}}>Maby & Joel</h1>
+                </div>
+
+                <div className="couple-photo-container">
+                    <img 
+                        src={M_J_pic}
+                        alt="Maby y Joel"
+                        className="couple-photo"
+                    />
+                </div>
+
                 <div className="content-area">
                     <p className="invitacion-texto">Tenemos el honor de invitarlos a nuestra boda que se llevará a cabo el día</p>
                     <h2>Sábado 20 de Diciembre de 2025</h2>
